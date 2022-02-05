34d0ac381d9c709eea441f59bae33db55ef9458f
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2022-02-05 08:14:25 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const app = express();
 // @ts-ignore
-const stripe = require('stripe')('sk_test_51KB4AqHzyheHHaqUvBTsbgzymrNbpsSON0CRTtBAehhr5T3EnuVZTQmEB7FVdoeMAk9WGonE8KGThs7nCiEOzZ8D00hceShXui');
+const stripe = require('stripe')('INSERT SECRET KEY HERE');
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
